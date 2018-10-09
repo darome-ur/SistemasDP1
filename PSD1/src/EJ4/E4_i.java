cb523994ddd0672044512141324407dd7e012dcc
darome-ur <darome@unirioja.es>
2018-10-09 20:27:11 +0200

    Ej4 parte 1 terminada y comenzada la segunda de tres

--- PSD1/src/EJ4/E4_i.java
+++ PSD1/src/EJ4/E4_i.java
@@ -1,7 +1,28 @@
 package EJ4;
 
-public class E4_i {
-	
-	
+import java.io.*;
 
+public class E4_i 
+{
+	
+	public static void main(String[] args) 
+	{
+		try {
+			File fichero = new File("fichero.txt");
+			String contenido = "Lapiz a 1€.";
+		
+			FileOutputStream fos = new FileOutputStream(fichero);
+			Writer w = new OutputStreamWriter(fos,"UTF-8");
+			w.write(contenido); //grabarlo en el writer
+			w.flush();//manda al fichero
+			w.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
---
+++ PSD1/src/EJ4/E4_ii.java
@@ -0,0 +1,12 @@
+package EJ4;
+
+import java.io.*;
+
+public class E4_ii {
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
