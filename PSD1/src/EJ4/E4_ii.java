60bd9e47a4738c9df3612cf84c9ed4753b8eb1a6
David Romé Lalanza <David Rom Lalanza@DESKTOP-VNFKK6Q>
2018-10-12 20:57:36 +0200

    Partres 2 del 4 a medio acabar

--- PSD1/src/EJ4/E4_ii.java
+++ PSD1/src/EJ4/E4_ii.java
@@ -6,7 +6,38 @@ public class E4_ii {
 	
 	public static void main(String[] args)
 	{
-		
+		// Parte 1:
+		try
+		{
+			File f = new File("fichero.txt");
+			FileInputStream is = new FileInputStream(f);
+			byte[] leido = new byte[1024];
+			int leidos;
+			while((leidos=is.read(leido)) != -1)
+			{
+				System.out.println(leidos);
+				leidos = is.read(leido);
+			}
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		// Parte 2:
+			try
+			{
+				File f = new File("fichero.txt");
+				FileReader fr = new FileReader(f);
+				BufferedReader br = new BufferedReader(fr);
+				String linea;
+				int leidos;
+				while((linea=br.readLine()) != null)
+				{
+					System.out.println(linea);
+				}
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
 	}
 
 }
