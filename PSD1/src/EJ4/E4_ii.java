49c3c5d9c06aab1d5fbb265c0a13b4df088944df
David Romé Lalanza <David Rom Lalanza@DESKTOP-VNFKK6Q>
2018-10-13 11:44:48 +0200

    Ejercicio 4 parte 1 terminada completamente

--- PSD1/src/EJ4/E4_ii.java
+++ PSD1/src/EJ4/E4_ii.java
@@ -7,9 +7,10 @@ public class E4_ii {
 	public static void main(String[] args)
 	{
 		// Parte 1:
+		File f = new File("fichero.txt");
 		try
 		{
-			File f = new File("fichero.txt");
+			
 			FileInputStream is = new FileInputStream(f);
 			byte[] leido = new byte[1024];
 			int leidos;
@@ -22,22 +23,38 @@ public class E4_ii {
 		catch(IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println("Comienza parte 2: ");
 		// Parte 2:
 			try
 			{
-				File f = new File("fichero.txt");
-				FileReader fr = new FileReader(f);
-				BufferedReader br = new BufferedReader(fr);
+				FileInputStream fis = new FileInputStream(f);
+				BufferedReader r = new BufferedReader(new InputStreamReader(fis,"UTF-8"));
 				String linea;
-				int leidos;
-				while((linea=br.readLine()) != null)
+				while((linea = r.readLine()) != null)
 				{
 					System.out.println(linea);
+					linea = r.readLine();
 				}
 			}
 			catch(IOException e) {
 				e.printStackTrace();
 			}
+			System.out.println("Comienza parte 3: ");
+			
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			BufferedReader r = new BufferedReader(new InputStreamReader(fis,"windows-1252"));
+			String linea;
+			while((linea = r.readLine()) != null)
+			{
+				System.out.println(linea);
+				linea = r.readLine();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
+	//Se lee correctamente la codificación en UTF-8.
+	//Ya que al tener mas bytes por caracter tiene mayor numero de simbolos  lo que incluye el del euro.
 
 }
