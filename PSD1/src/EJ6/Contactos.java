5330627d9119472d951372c863ff027a3faac176
darome <darome@CCTL139-13.unirioja.loc>
2018-10-23 19:44:39 +0200

    Ejercicio deserializar a medias

--- PSD1/src/EJ6/Contactos.java
+++ PSD1/src/EJ6/Contactos.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 public class Contactos implements Serializable{
 	
-	public static class Datos {
+	public static class Datos implements Serializable{
 		 private String email;
 		 private String tfno;
 		 public Datos(String email, String telefono) {
@@ -19,18 +19,26 @@ public class Contactos implements Serializable{
 		 public String getEmail() {
 		 return email;
 		 }
-		 }
+	}
 
 		 private Map<String, Datos> agenda = new HashMap<String, Datos>();
+		 
+		 private int TamañoMAximoAgenda = 500;
+		 
 		 public void addDatos(String nombre, String tfno, String email) {
 		 agenda.put(nombre, new Datos(email, tfno));
 		 }
+		 
 		 public String getTfno(String nombre) {
 		 return agenda.get(nombre).getTfno();
 		 }
 		public String getEmail(String nombre) {
 		 return agenda.get(nombre).getEmail();
 		 }
+		public int tamaño()
+		{
+			return 0;
+		}
 
 
 }
--- PSD1/src/EJ6/Principal.java
+++ PSD1/src/EJ6/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		//Datos contacto1 = new Datos("dromelalanza@gmail.com","690024227");
 		
-		try
+		/*try
 		{
 			Contactos c = new Contactos();
 			c.addDatos("David", "690024227", "dromelalanza@gmail.com");
@@ -15,10 +15,29 @@ public class Principal {
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			oos.writeObject(c);
 			oos.close();
-			
 		}
 		catch(IOException e)
 		{
+			e.printStackTrace();
+		}*/
+		
+		try
+		{
+			FileInputStream fis = new FileInputStream("Ejercicio6.txt");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Contactos c2 = new Contactos();
+			c2 =  (Contactos) ois.readObject();
+			;
+			
+			System.out.println("email:"+c2.getEmail("David")+"Tlf:"+c2.getTfno("David"));
+			ois.close();
+				
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
